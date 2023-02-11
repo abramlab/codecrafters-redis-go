51a807f77c9088bc882b4d0586ef54efe0344f42
Misha Abramovich <abram13504@yandex.ru>
2023-02-11 22:50:54 +0100

    2 stage

--- app/server.go
+++ app/server.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
@@ -19,9 +18,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		pong := []byte("+PONG\r\n")
+		_, err = conn.Write(pong)
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 }
