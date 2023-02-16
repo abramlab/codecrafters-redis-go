65c45bca1ab7ab95f5916bd187fbfc39fa22f994
Misha Abramovich <abram13504@yandex.ru>
2023-02-16 21:59:27 +0100

    3 stage

--- app/server.go
+++ app/server.go
@@ -15,20 +15,22 @@ func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		exitWithError(fmt.Errorf("failed to bind to port 6379: %s", err))
 	}
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			exitWithError(fmt.Errorf("failed to accept connection: %s", err))
 		}
 		pong := []byte("+PONG\r\n")
 		_, err = conn.Write(pong)
 		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
+			exitWithError(fmt.Errorf("failed to write to connection: %s", err))
 		}
 	}
+}
+
+func exitWithError(err error) {
+	fmt.Println(err.Error())
+	os.Exit(1)
 }
