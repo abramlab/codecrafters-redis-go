abb30083fc3e59298f8647043fcc48a770dbf577
Misha Abramovich <abram13504@yandex.ru>
2023-02-18 20:10:20 +0100

    5 stage

--- app/server.go
+++ app/server.go
@@ -1,13 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
 func main() {
@@ -33,14 +31,27 @@ func handleConnection(conn net.Conn) {
 
 	for {
 		buf := make([]byte, 1024)
-		if _, err := conn.Read(buf); err != nil {
+		n, err := conn.Read(buf)
+		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			exitWithError(fmt.Errorf("failed to read from connection: %s", err))
 		}
-		pong := []byte("+PONG\r\n")
-		if _, err := conn.Write(pong); err != nil {
+		buf = buf[:n]
+
+		fmt.Printf("Raw request: %q\n", string(buf))
+
+		echo, err := parseSimpleEchoCommand(buf)
+		if err != nil {
+			errResp := []byte(fmt.Sprintf("-ERR %s: %q\r\n", err, string(buf)))
+			if _, err = conn.Write(errResp); err != nil {
+				exitWithError(fmt.Errorf("failed to write to connection: %s", err))
+			}
+			continue
+		}
+
+		if _, err := conn.Write(echo.response()); err != nil {
 			exitWithError(fmt.Errorf("failed to write to connection: %s", err))
 		}
 	}
@@ -49,4 +60,28 @@ func handleConnection(conn net.Conn) {
 func exitWithError(err error) {
 	fmt.Println(err.Error())
 	os.Exit(1)
+}
+
+type echoCommand struct {
+	arg string
+}
+
+func (c *echoCommand) response() []byte {
+	return []byte(fmt.Sprintf("+%s\r\n", c.arg))
+}
+
+func parseSimpleEchoCommand(raw []byte) (*echoCommand, error) {
+	parts := bytes.Split(bytes.TrimSuffix(raw, []byte("\r\n")), []byte("\r\n"))
+
+	if len(parts) != 5 {
+		return nil, fmt.Errorf("unsupported command")
+	}
+
+	if parts[0][0] != '*' {
+		return nil, fmt.Errorf("unsupported command")
+	}
+	if !(bytes.Equal(parts[1], []byte("$4")) && bytes.Equal(parts[2], []byte("ECHO"))) {
+		return nil, fmt.Errorf("unsupported command")
+	}
+	return &echoCommand{arg: string(parts[4])}, nil
 }
